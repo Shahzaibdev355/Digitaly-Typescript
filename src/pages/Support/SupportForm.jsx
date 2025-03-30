7f518efb4cbdeee593c119a34938afbc1c82759e
Shahzaib <shahzaibsheikh355@gmail.com>
2025-03-30 19:53:08 +0500

    testing

--- src/pages/Support/SupportForm.jsx
+++ src/pages/Support/SupportForm.jsx
@@ -316,7 +316,7 @@ const SupportForm = () => {
         )}
       </div>
 
-      <div className="col-md-12   contact-us-input-objet" style={{border: '3px solid red'}}>
+      <div className="col-md-12   contact-us-input-objet" >
         <Label htmlFor="category" className="contact-form-input-label">
           {" "}
           Catégorie <span>*</span>{" "}
